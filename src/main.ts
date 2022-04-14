8bb99f8d1e12208f6b91392780529cece7c5cc47
Tim Repke <repke@mcc-berlin.net>
2022-04-14 15:38:47 +0200

    add logo

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+
+// eslint-disable-next-line no-unused-vars
+var app=createApp(App).mount('#app')
+setTimeout(() => {
+app.$
+    App.components.NacsosLogo.methods.setAnimationState(true);
+}, 2000)
