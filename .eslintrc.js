4463d64ea00f84d743b0ae581a8f711ba715965b
Tim Repke <repke@mcc-berlin.net>
2022-04-14 16:33:52 +0200

    adapt lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['error', {
+      code: 140,
+      ignorePattern: '(d|style)="[^"]*"',
+      ignoreStrings: true,
+      ignoreTrailingComments: true,
+    }],
   },
 };
