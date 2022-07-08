e4264404f697243769b65127b71857cf6bd596e1
Tim Repke <repke@mcc-berlin.net>
2022-07-08 18:18:57 +0200

    add tooltip component

---
+++ src/components/InlineToolTip.vue
@@ -0,0 +1,21 @@
+<template>
+  <template v-if="!info">
+    <slot/>
+  </template>
+  <template v-else>
+    <span class="nacsos-tooltip">
+      <slot/>
+      <span class="nacsos-tooltiptext small">
+        {{ info }}
+      </span>
+    </span>
+  </template>
+</template>
+
+<script>
+
+export default {
+  name: 'InlineToolTip',
+  props: ['info'],
+};
+</script>
