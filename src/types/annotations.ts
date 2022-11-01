df748c8738e64f21ee99b26fcf231c878ea8c099
Tim Repke <repke@mcc-berlin.net>
2022-11-01 15:14:51 +0100

    add helper types and colormaps

---
+++ src/types/annotations.ts
@@ -0,0 +1,13 @@
+export type AnnotationValueTuple = [
+  v_int: number,
+  v_float: number,
+  v_bool: boolean,
+  v_str: string,
+];
+
+export enum Value {
+  V_INT,
+  V_FLOAT,
+  V_BOOL,
+  V_STR,
+}
