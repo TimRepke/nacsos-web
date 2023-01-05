9d6cbdf7c0ee214911cd3dbd3161f0c87bf0c8f4
Tim Repke <repke@mcc-berlin.net>
2023-01-05 17:22:41 +0100

    update nacsos-core code

--- src/plugins/api/api-core/models/Event.ts
+++ src/plugins/api/api-core/models/Event.ts
@@ -9,15 +9,16 @@ import type { PipelineTaskStatusStartedEvent } from './PipelineTaskStatusStarted
 
 export type Event = {
   event: Event.event;
-  payload: (PipelineTaskStatusFailedEvent | PipelineTaskStatusChangedEvent | PipelineTaskStatusCompletedEvent | PipelineTaskStatusStartedEvent);
+  payload: (PipelineTaskStatusCompletedEvent | PipelineTaskStatusStartedEvent | PipelineTaskStatusFailedEvent | PipelineTaskStatusChangedEvent);
 };
 
 export namespace Event {
 
   export enum event {
+    PIPELINE_TASK_STATUS_COMPLETED_EVENT = 'PipelineTaskStatusCompletedEvent',
+    PIPELINE_TASK_STATUS_STARTED_EVENT = 'PipelineTaskStatusStartedEvent',
     PIPELINE_TASK_STATUS_FAILED_EVENT = 'PipelineTaskStatusFailedEvent',
     PIPELINE_TASK_STATUS_CHANGED_EVENT = 'PipelineTaskStatusChangedEvent',
-    PIPELINE_TASK_STATUS_COMPLETED_EVENT = 'PipelineTaskStatusCompletedEvent',
   }
 
 
