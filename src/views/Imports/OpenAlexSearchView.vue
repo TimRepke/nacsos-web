1f7d3eec465b25793b3d5afca7bb3c66cf64784f
Tim Repke <repke@mcc-berlin.net>
2023-08-04 11:55:17 +0200

    add solr search view

---
+++ src/views/Imports/OpenAlexSearchView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>OpenAlex Solr Search</h2>
+    pass
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'OpenAlexSearchView',
+  data() {
+    return {
+      // pass
+    };
+  },
+  async mounted() {
+    // pass
+  },
+  methods: {
+    // pass
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
