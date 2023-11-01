c271a8da86ba25fcc75fffb9dfeecf64d6f37d27
Tim Repke <repke@mcc-berlin.net>
2023-11-01 15:08:27 +0100

    make generic items better

--- src/components/items/GenericItem.vue
+++ src/components/items/GenericItem.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="card m-2 p-0 text-start">
+  <div class="card m-2 p-0 text-start w-100">
     <div class="card-body">
       <p class="card-text" v-html="htmlText" />
     </div>
     <div class="card-footer d-flex justify-content-between small text-muted" v-if="item.meta">
+      <div class="position-absolute end-0">
+        <font-awesome-icon :icon="['fas', 'info-circle']" class="me-2" @click="showAll = !showAll" />
+      </div>
       <ul class="list-unstyled">
         <template v-for="(value, key) in item.meta" :key="key">
           <li v-if="!key.startsWith('_')">
             <strong>{{ key }}:</strong> {{ value }}
           </li>
+        </template>
+        <template v-if="showAll">
+          <div>
+            <strong>Item ID: </strong>
+            <code>
+              {{ item.item_id }}
+            </code>
+          </div>
+          <template v-for="(value, key) in item.meta" :key="key">
+            <li v-if="key.startsWith('_')" class="text-muted small">
+              <strong>{{ key }}:</strong> {{ value }}
+            </li>
+          </template>
         </template>
       </ul>
     </div>
@@ -35,6 +51,11 @@ export default defineComponent({
       required: false,
       default: undefined,
     },
+  },
+  data() {
+    return {
+      showAll: false,
+    };
   },
   computed: {
     htmlText() {
