20426c40ce9c3441b151c323e4eb2a94aa196302
Tim Repke <repke@mcc-berlin.net>
2023-12-20 16:48:50 +0100

    pimp my parseview

--- src/views/ParseView.vue
+++ src/views/ParseView.vue
@@ -1,45 +1,90 @@
 <template>
   <div>
-    <div>Hi</div>
-    <textarea v-model="query"></textarea>
-    <div>{{ count }}</div>
-    <button @click="ask">query</button>
-    <pre>{{ tree }}</pre>
+    <h2>NQL Query parser</h2>
+
+    <NQLBox v-model:query="queryStr" class="m-4 ms-1" />
+
+    <hr />
+
+    <div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="output" id="out-py" value="python" v-model="output" />
+        <label class="form-check-label" for="out-py">Python</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="output"
+          id="out-pyc"
+          value="python-compact"
+          v-model="output"
+        />
+        <label class="form-check-label" for="out-pyc">Python (compact)</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="output" id="out-json" value="json" v-model="output" />
+        <label class="form-check-label" for="out-json">JSON</label>
+      </div>
+    </div>
+
+    <div class="card p-1" style="color: #559e55">
+      <pre><code>{{ dump }}</code></pre>
+    </div>
+
+    <hr />
+    <button @click="ask" class="btn btn-outline-secondary">Test query</button>
+    <div>Number of results: {{ count }}</div>
+
+    <hr />
+    <h4>Parse table</h4>
+    <ul>
+      <li v-for="step in table.slice().reverse()" :key="step.index">
+        Step {{ step.index }} wants:
+        <ul>
+          <li v-for="(value, key) in step.wants" :key="key">
+            <strong>{{ key }}: </strong> <code>{{ value[0]?.rule }}</code>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { debug } from "@/util/nql";
+import { parse, parseHist, debug } from "@/util/nql";
 import { currentProjectStore } from "@/stores";
 import { API, logReject } from "@/plugins/api";
+import NQLBox from "@/components/NQLBox.vue";
 
 export default defineComponent({
-  name: "HomeView",
+  name: "ParseView",
+  components: { NQLBox },
   data() {
-    let res;
-    res = debug('title: "hi"');
-    // console.log(res);
-    res = debug("PY: >2020");
-    // console.log(res);
-    res = debug('PY: >2020\n  OR\n  title: "carbon"\n');
-
-    res = debug("import: abc, 12d, -af3");
-
-    debug("label: key=3 FROM abc, 12d");
-    debug("label: key=3");
-    debug("label: key==3,4,5");
-    debug("label: key !> 3,4,5");
-    debug("label: key !> 3,4,5 FROM abc, -12d");
-    debug("label: key <= 5 BY ~cde,123");
-    debug("label: key > 2 FROM abc, -12d BY ~cde,123");
-
-    debug('meta: extra LIKE "% test %"');
-
-    debug("meta: extra <= 4");
-
-    debug("(PY:>2000 AND (PY:<2100 OR PY:>2200))");
-    debug("PY:>2000 AND PY:<2100 AND PY:>2200");
+    // let res;
+    // res = debug('title: "hi"');
+    // // console.log(res);
+    // res = debug("PY: >2020");
+    // // console.log(res);
+    // res = debug('PY: >2020\n  OR\n  title: "carbon"\n');
+    //
+    // res = debug("import: abc, 12d, -af3");
+    //
+    // debug("label: key=3 FROM abc, 12d");
+    // debug("label: key=3");
+    // debug("label: key==3,4,5");
+    // debug("label: key !> 3,4,5");
+    // debug("label: key !> 3,4,5 FROM abc, -12d");
+    // debug("label: key <= 5 BY ~cde,123");
+    // debug("label: key > 2 FROM abc, -12d BY ~cde,123");
+    //
+    // debug('meta: extra LIKE "% test %"');
+    //
+    // debug("meta: extra <= 4");
+    //
+    // debug("(PY:>2000 AND (PY:<2100 OR PY:>2200))");
+    // debug("PY:>2000 AND PY:<2100 AND PY:>2200");
 
     // import: 944f2985-503b-4aad-8af1-e48b694eba19
 
@@ -48,13 +93,14 @@ export default defineComponent({
 
     // console.log(res);
     return {
-      query: "import: abc, 12d, -af3",
+      queryStr: 'title: "%hi%"',
       count: -1,
+      output: "json",
     };
   },
   methods: {
     ask() {
-      const query = debug(this.query);
+      const query = debug(this.queryStr);
       this.count = -1;
       API.core.search
         .nqlQueryApiSearchNqlQueryPost({
@@ -62,14 +108,36 @@ export default defineComponent({
           requestBody: query[0],
         })
         .then((response) => {
-          this.count = response.data;
+          this.count = response.data.n_docs;
         })
         .catch(logReject);
     },
   },
   computed: {
-    tree() {
-      return JSON.stringify(debug(this.query), null, "  ");
+    dump() {
+      const query = parse(this.queryStr);
+      if (query.length === 0) {
+        return "[No result]";
+      }
+
+      if (this.output === "python") {
+        return `QUERY = NQLFilterParser.validate_python(json.loads('''\n${JSON.stringify(query[0], null, "  ")}\n'''))`;
+      } else if (this.output === "python-compact") {
+        return `QUERY = NQLFilterParser.validate_python(json.loads('${JSON.stringify(query[0])}'))`;
+      } else if (this.output === "json") {
+        return JSON.stringify(query, null, "  ");
+      } else {
+      }
+    },
+    table() {
+      try {
+        const parser = parseHist(this.queryStr);
+        if (!parser) return [];
+        console.log(parser.table);
+        return parser.table;
+      } catch (e) {
+        return [];
+      }
     },
   },
 });
